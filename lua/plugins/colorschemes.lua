0b9a7a4aa162dde26c29510bc49cb696b0ffab4e
michaelxilinas <mxilinas@kpmg.ca>
2026-07-07 08:56:28 -0700

    Added one-dark colorscheme and config.

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,7 +3,9 @@ return {
 	priority = 1000,
 	config = function()
 		require("onedark").setup({
-			style = "darker",
+			style = "warm", -- dark, darker, cool, deep, warm, warmer, light
+			toggle_style_key = "<leader>s",
+			toggle_style_list = { "warm", "light" },
 		})
 		require("onedark").load()
 	end,
